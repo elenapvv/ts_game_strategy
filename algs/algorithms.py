43e2ced9a793f0f99a7078c37b916a834c6c52f1
Elena Popkova <popkova@ipavlov.ai>
2022-03-14 13:37:37 +0300

    new results

---
+++ algs/algorithms.py
@@ -0,0 +1,78 @@
+import logging
+
+from math import exp, isinf
+
+logger = logging.getLogger(__name__)
+
+
+class Algorithms:
+    ETA = 0.0001  # оптимально
+
+    @classmethod
+    def hedge(cls, old_weights: list, shares: tuple, expert_gaines: list):
+        if not (len(old_weights) == len(shares) == len(expert_gaines)):
+            logger.error(f"Переданы списки разной длины\nold_weights = {old_weights}, shares = {shares}, "
+                         f"expert_gaines = {expert_gaines}")
+            exit()
+
+        old_weights_sum = sum(old_weights)
+        if isinf(old_weights_sum):
+            logger.error("Сумма весов равна бесконечности")
+            exit()
+
+        old_weights_normalized = [old_weight / old_weights_sum for old_weight in old_weights]
+
+        alg_gains_for_shares = [old_weights_normalized[i] * expert_gaines[i] for i in range(len(shares))]
+        # alg_gain = sum(alg_gains_for_shares)
+
+        logger.debug(f"alg_gains_for_shares={alg_gains_for_shares},\nold_weights_sum={old_weights_sum},\n"
+                     f"old_weights_normalized={old_weights_normalized},\nold_weights={old_weights}"
+                     f"\nexpert_gaines={expert_gaines}")
+
+        new_weights = []
+        try:
+            for i in range(len(shares)):
+                new_weights.append(old_weights[i] * exp(cls.ETA * expert_gaines[i]))
+
+        except OverflowError:
+            logger.error(f"Слишком большое для экспоненты число: {cls.ETA * expert_gaines[len(new_weights)]}")
+            raise
+        except Exception:
+            raise
+
+        return alg_gains_for_shares, new_weights
+
+    @classmethod
+    def fixed_share(cls, old_weights: list, shares: tuple, expert_losses: list, probabilities: list):
+        if not (len(old_weights) == len(shares) == len(expert_losses)):
+            logger.error(f"Переданы списки разной длины\nold_weights = {old_weights}, shares = {shares}, "
+                         f"expert_losses = {expert_losses}")
+            exit()
+
+        old_weights_sum = sum(old_weights)
+        if isinf(old_weights_sum):
+            logger.error("Сумма весов равна бесконечности")
+            exit()
+
+        old_weights_normalized = [old_weight / old_weights_sum for old_weight in old_weights]
+
+        alg_loss = sum([old_weights_normalized[i] * expert_losses[i] for i in range(len(shares))])
+
+        logger.debug(f"alg_loss={alg_loss},\nold_weights_sum={old_weights_sum},\n"
+                     f"old_weights_normalized={old_weights_normalized},\nold_weights={old_weights}"
+                     f"\nexpert_losses={expert_losses}")
+
+        new_weights = []
+        try:
+            for i in range(len(shares)):
+                overridden_weight = old_weights[i] * exp(- cls.ETA * expert_losses[i])
+                pool = sum([probabilities[i] * expert_losses[i] for i in range(len(shares))])
+                new_weights.append((1 - probabilities[i]) * overridden_weight + 1 / (len(shares) - 1))
+
+        except OverflowError:
+            logger.error(f"Слишком большое для экспоненты число: {- cls.ETA * expert_losses[len(new_weights)]}")
+            raise
+        except Exception:
+            raise
+
+        return alg_loss, new_weights
--- data_operations/trader.py
+++ data_operations/trader.py
@@ -3,7 +3,7 @@ import logging
 import os
 import matplotlib.pyplot as plt
 
-from algs.hedge import Hedge
+from algs.algorithms import Algorithms
 from utils.manager_db import DATE_TMPL, SharesDB
 from progress.bar import IncrementalBar
 
@@ -13,25 +13,34 @@ logging.basicConfig(
     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-TO_SAVE_LOGS = True
+TO_SAVE_LOGS = False
 TO_SAVE_PLOTS = True
 
 # можем тут переопределить параметр эта
-Hedge.ETA = 0.00001
+Algorithms.ETA = 0.00001
 
 CASH = 10000
 
-# logs_filename = f'../results/ETA={Hedge.ETA}, time={datetime.datetime.now().strftime("%d.%m.%Y %H.%M.%S")}.txt'
-filename = f'ETA={Hedge.ETA}, CASH={CASH} with calculated number of shares'
-subdir_name = 'with calculated number of shares - 4/'
-logs_filename = f'../results/{subdir_name}{filename}.txt'
-plots_filename = f'../plots/{subdir_name}{filename}.png'
+# названия файлов и директорий
+filename = f'cumulative gains for all tickers and common(ETA={Algorithms.ETA})'
+
+subdir_name = 'cumulative gains for all tickers and common - 11/'
+
+logs_dir = f'../results/{subdir_name}'
+plots_dir = f'../plots/{subdir_name}'
+
+logs_filename = f'{logs_dir}{filename}.txt'
+plots_filename = f'{plots_dir}{filename}.png'
 
 if TO_SAVE_LOGS and not os.path.isfile(logs_filename):
+    if not os.path.exists(logs_dir):
+        os.makedirs(logs_dir)
     with open(logs_filename, 'w'):
         pass
 
 if TO_SAVE_PLOTS and not os.path.isfile(plots_filename):
+    if not os.path.exists(plots_dir):
+        os.makedirs(plots_dir)
     with open(plots_filename, 'w'):
         pass
 
@@ -39,14 +48,14 @@ MIN_DATE = datetime.datetime.strptime('01/06/07', DATE_TMPL)
 MAX_DATE = datetime.datetime.strptime('15/01/22', DATE_TMPL)  # не включительно
 
 
-def calculate_tickers_numbers(func_weights, func_step_shares):
+def calculate_tickers_numbers(func_weights, func_step_shares, func_general_cash):
     func_weights_sum = sum(func_weights)
     tickers_numbers = []
     remainder = 0
 
     for ticker_idx in range(shares_num):
         # сколько денег готовы выделить на текущую акцию
-        cash_on_ticker = CASH * func_weights[ticker_idx] / func_weights_sum + remainder
+        cash_on_ticker = func_general_cash * func_weights[ticker_idx] / func_weights_sum + remainder
         tickers_number = int(cash_on_ticker / func_step_shares[ticker_idx])
         tickers_numbers.append(tickers_number)
 
@@ -62,9 +71,11 @@ if __name__ == '__main__':
         logger.error(f"Не удалось получить названия тикеров. Получено: {columns}")
         exit()
 
-    shares_num = len(columns) - 2
+    columns = [column[3] for column in columns[2:]]
+    shares_num = len(columns)
     weights = [1 / shares_num for _ in range(shares_num)]
-    expert_losses = [0 for _ in range(shares_num)]
+    expert_gaines = [0 for _ in range(shares_num)]
+    general_cash_list = [CASH, ]
 
     considered_date = MIN_DATE
     old_time_step_shares = SharesDB.get_shares_by_date(date=considered_date, shares_num=shares_num)
@@ -77,60 +88,78 @@ if __name__ == '__main__':
     logger.info(f"Сначала покупаем по {number_of_share} тикера(-ов)")
     number_of_shares = [number_of_share for _ in range(shares_num)]
     considered_date = considered_date + datetime.timedelta(days=1)
+    tickers_cash_lists = [[number_of_share * share, ] for share in old_time_step_shares]
+    number_of_shares_lists = [[number_of_share, ] for _ in range(shares_num)]
 
     step = 1
     cumulative_loss = 0
-    alg_losses = []
+    alg_gains = []
+    alg_gains_for_shares_list = [[number_of_share * weights[0], ] for _ in range(shares_num)]
 
-    logger.info(f"ETA={Hedge.ETA}")
+    logger.info(f"ETA={Algorithms.ETA}")
 
     bar = IncrementalBar('Выполнение...', max=(MAX_DATE - MIN_DATE).days)
 
     while considered_date < MAX_DATE:
-
         new_time_step_shares = SharesDB.get_shares_by_date(date=considered_date, shares_num=shares_num)
 
-        expert_losses = [number_of_shares[share_idx] * (new_time_step_shares[share_idx] -
+        expert_gaines = [number_of_shares[share_idx] * (new_time_step_shares[share_idx] -
                                                         old_time_step_shares[share_idx]) for share_idx
                          in range(shares_num)]
 
-        alg_loss, weights = Hedge.calculate_new_weights(old_weights=weights, shares=new_time_step_shares,
-                                                        expert_losses=expert_losses)
+        alg_gains_for_shares, weights = Algorithms.hedge(old_weights=weights, shares=new_time_step_shares,
+                                                         expert_gaines=expert_gaines)
 
-        log_text = f"Потери алгоритма Hedge на шаге {step} ({considered_date}): {alg_loss}"
+        alg_gain = sum(alg_gains_for_shares)
+        log_text = f"Выигрыш алгоритма Hedge на шаге {step} ({considered_date}): {alg_gain}"
         logger.debug(log_text)
-        cumulative_loss += alg_loss
-        if TO_SAVE_LOGS:
-            with open(logs_filename, 'a+') as f:
-                f.write(f"{log_text}, всего: {cumulative_loss}\n")
-        alg_losses.append(cumulative_loss)
+        cumulative_loss += alg_gain
+        # if TO_SAVE_LOGS:
+        #     with open(logs_filename, 'a+') as f:
+        #         f.write(f"{log_text}, всего: {cumulative_loss}\n")
+        alg_gains.append(cumulative_loss)
+        for share_idx in range(shares_num):
+            alg_gains_for_shares_list[share_idx].append(
+                alg_gains_for_shares_list[share_idx][-1] + alg_gains_for_shares[share_idx])
 
         considered_date = considered_date + datetime.timedelta(days=1)
         old_time_step_shares = new_time_step_shares
         step += 1
 
         # считаем, сколько купим акций, исходя из весов
-        number_of_shares = calculate_tickers_numbers(func_weights=weights, func_step_shares=old_time_step_shares)
+        general_cash = sum([number_of_shares[i] * old_time_step_shares[i] for i in range(len(old_time_step_shares))])
+        # general_cash_list.append(general_cash)
+        # for ticker_cash_list_idx in range(shares_num):
+        #     tickers_cash_lists[ticker_cash_list_idx].append(
+        #         number_of_shares[ticker_cash_list_idx] * old_time_step_shares[ticker_cash_list_idx])
 
-        # print(old_time_step_shares, number_of_shares)
+        number_of_shares = calculate_tickers_numbers(func_weights=weights, func_step_shares=old_time_step_shares,
+                                                     func_general_cash=general_cash)
+        # for share_idx in range(shares_num):
+        #     number_of_shares_lists[share_idx].append(number_of_shares[share_idx])
 
         bar.next()
 
     bar.finish()
 
-    log_text = f"Кумулятивные потери алгоритма Hedge для ETA={Hedge.ETA}: {cumulative_loss}"
+    log_text = f"Кумулятивные выигрыши алгоритма Hedge для ETA={Algorithms.ETA}: {cumulative_loss}"
     logger.info(log_text)
 
     if TO_SAVE_LOGS:
         with open(logs_filename, 'a+') as f:
-            f.write(f"\n{log_text}")
+            f.write(f"Итоговый выигрыш - {general_cash}")
             logger.info("Логи с результатами сохранены")
 
     if TO_SAVE_PLOTS:
-        plt.plot(alg_losses)
+        plt.plot(alg_gains, label="Для всех котировок")
+        for share_idx in range(shares_num):
+            plt.plot(alg_gains_for_shares_list[share_idx], label=columns[share_idx])
         plt.xlabel("Шаги")
-        plt.ylabel(f"Кумулятивные потери для каждого шага")
-        plt.title(f"Кумулятивные потери алгоритма Hedge для ETA={Hedge.ETA}")
+        # plt.ylabel(f"Количество акций")
+        # plt.title(f"Изменение количества акций для ETA={Algorithms.ETA}")
+        plt.ylabel(f"Кумулятивный выигрыш для каждого шага")
+        plt.title(f"Кумулятивные выигрыши алгоритма Hedge для ETA={Algorithms.ETA}")
+        plt.legend()
         plt.savefig(plots_filename)
 
         logger.info("Графики с результатами сохранены")
